fe07d2e32678dae4615fbfb6cc576a877839ce07
Constantin Müller <info@mueller-constantin.de>
2024-11-03 20:12:01 +0100

    feat: support running processes in usermode

--- kernel/src/system/process.c
+++ kernel/src/system/process.c
@@ -37,34 +37,6 @@ process_t* process_create(const char* name, const char* path) {
 
     file_close(executable_fd);
 
-    // Load the executable
-
-    if(!elf_is_valid(executable_data, executable_stat.size)) {
-        kfree(executable_data);
-        uart_16550_write(UART_16550_COM1, "Invalid ELF file\n", 17);
-        return NULL;
-    }
-
-    page_directory_t* former_address_space = vmm_get_current_address_space();
-    page_directory_t* address_space = vmm_create_address_space();
-
-    if(address_space == NULL) {
-        kfree(executable_data);
-        uart_16550_write(UART_16550_COM1, "Failed to create address space\n", 29);
-        return NULL;
-    }
-
-    vmm_switch_address_space(address_space);
-
-    if(elf_load(executable_data, executable_stat.size) != 0) {
-        kfree(executable_data);
-        vmm_switch_address_space(former_address_space);
-        uart_16550_write(UART_16550_COM1, "Failed to load ELF file\n", 22);
-        return NULL;
-    }
-
-    vmm_switch_address_space(former_address_space);
-
     // Create the process
 
     process_t* process = (process_t*) kmalloc(sizeof(process_t));
@@ -74,7 +46,6 @@ process_t* process_create(const char* name, const char* path) {
     }
 
     process->pid = process_next_pid();
-    process->address_space = address_space;
 
     process->name = (char*) kmalloc(strlen(name) + 1);
 
@@ -92,22 +63,90 @@ process_t* process_create(const char* name, const char* path) {
 
     strcpy(process->path, path);
 
+    // Load the executable
+
+    if(!elf_is_valid(executable_data, executable_stat.size)) {
+        kfree(process->name);
+        kfree(process->path);
+        kfree(process);
+        kfree(executable_data);
+        uart_16550_write(UART_16550_COM1, "Invalid ELF file\n", 17);
+        return NULL;
+    }
+
+    page_directory_t* former_address_space = vmm_get_current_address_space();
+    page_directory_t* address_space = vmm_create_address_space();
+
+    if(address_space == NULL) {
+        kfree(process->name);
+        kfree(process->path);
+        kfree(process);
+        kfree(executable_data);
+        uart_16550_write(UART_16550_COM1, "Failed to create address space\n", 29);
+        return NULL;
+    }
+
+    process->address_space = address_space;
+
+    // Temporarily switch to the new address space to load the executable
+    vmm_switch_address_space(address_space);
+
+    if(elf_load(executable_data, executable_stat.size) != 0) {
+        kfree(process->name);
+        kfree(process->path);
+        kfree(process);
+        kfree(executable_data);
+        vmm_switch_address_space(former_address_space);
+        uart_16550_write(UART_16550_COM1, "Failed to load ELF file\n", 22);
+        return NULL;
+    }
+
+    // Allocate the user stack
+
+    void* user_stack = vmm_map_memory(NULL, PAGE_SIZE, NULL, false, true);
+
+    if(user_stack == NULL) {
+        KPANIC(KPANIC_VMM_OUT_OF_USER_SPACE_MEMORY_CODE, KPANIC_VMM_OUT_OF_USER_SPACE_MEMORY_MESSAGE, NULL);
+    }
+
+    process->stack = (void*) ((uint32_t) user_stack + PAGE_SIZE - 1);
+
+    // Switch back to former address space after loading the executable
+    vmm_switch_address_space(former_address_space);
+
     process->context.eip = (uint32_t) elf_get_entry_point(executable_data, executable_stat.size);
 
     kfree(executable_data);
-
-    process->stack = vmm_map_memory(NULL, PAGE_SIZE, NULL, false, true);
-
-    if(!process->stack) {
-        KPANIC(KPANIC_VMM_OUT_OF_USER_SPACE_MEMORY_CODE, KPANIC_VMM_OUT_OF_USER_SPACE_MEMORY_MESSAGE, NULL);
-    }
 
     return process;
 }
 
 void process_run(process_t* process) {
     vmm_switch_address_space(process->address_space);
-    ((void (*)()) process->context.eip)(); 
+
+    const uint32_t USER_DS_SELECTOR = 0x23;
+    const uint32_t USER_CS_SELECTOR = 0x1B;
+
+    __asm__ volatile (
+        "cli\n"
+        "mov %0, %%ds\n"
+        "mov %0, %%es\n"
+        "mov %0, %%fs\n"
+        "mov %0, %%gs\n"
+        "pushl %1\n" // User Stack Segment
+        "pushl %2\n" // User Stack Pointer
+        "pushl %3\n" // EFLAGS
+        "pushl %4\n" // User Code Segment
+        "pushl %5\n" // EIP
+        "iret\n"
+        :
+        : "r"(USER_DS_SELECTOR),
+          "r"(USER_DS_SELECTOR),
+          "r"(process->stack),
+          "r"(0x200),
+          "r"(USER_CS_SELECTOR),
+          "r"(process->context.eip)
+    );
 }
 
 static pid_t process_next_pid() {
